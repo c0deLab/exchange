3035eb7eb674988605ac865a67420d570613e13a
Lukas Hermann <lukashermann28@gmail.com>
2019-05-15 17:32:02 -0400

    Updates

--- precache-manifest.2de5bf292a86da9d1774c1d495abcaa5.js
+++ precache-manifest.2de5bf292a86da9d1774c1d495abcaa5.js
@@ -1,262 +1,262 @@
 self.__precacheManifest = [
   {
     "revision": "5a48dd1ce8160901f264201c0cf9701b",
-    "url": "/exchange/static/media/34.5a48dd1c.jpg"
+    "url": "/static/media/34.5a48dd1c.jpg"
   },
   {
-    "revision": "8a48e0828459d7e06e6c",
-    "url": "/exchange/static/css/main.85439852.chunk.css"
+    "revision": "da1530b5107feacff600",
+    "url": "/static/css/main.85439852.chunk.css"
   },
   {
-    "revision": "95d3f3c5f08cdd3f41f4",
-    "url": "/exchange/static/js/runtime~main.c3814968.js"
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
   },
   {
     "revision": "818fd75c3153cf85d558",
-    "url": "/exchange/static/js/2.fc62f515.chunk.js"
+    "url": "/static/js/2.fc62f515.chunk.js"
   },
   {
     "revision": "56b9c11f65587f84f46e3dfec4942381",
-    "url": "/exchange/static/media/0.56b9c11f.jpg"
+    "url": "/static/media/0.56b9c11f.jpg"
   },
   {
     "revision": "d860fadd63c8924db9fd12eab81d3b5c",
-    "url": "/exchange/static/media/1.d860fadd.jpg"
+    "url": "/static/media/1.d860fadd.jpg"
   },
   {
     "revision": "8377e4cf1268be09082a7852b07edc0d",
-    "url": "/exchange/static/media/10.8377e4cf.jpg"
+    "url": "/static/media/10.8377e4cf.jpg"
   },
   {
     "revision": "9cda3418c0306def959a9fed86e3ac91",
-    "url": "/exchange/static/media/11.9cda3418.jpg"
+    "url": "/static/media/11.9cda3418.jpg"
   },
   {
     "revision": "42877c15be9587f801428158c261af47",
-    "url": "/exchange/static/media/12.42877c15.jpg"
+    "url": "/static/media/12.42877c15.jpg"
   },
   {
     "revision": "8c0fadaaa4f8b7bc0475339e0008fdc1",
-    "url": "/exchange/static/media/13.8c0fadaa.jpg"
+    "url": "/static/media/13.8c0fadaa.jpg"
   },
   {
     "revision": "aba1ad492b48b5e692611735a5a4ae27",
-    "url": "/exchange/static/media/14.aba1ad49.jpg"
+    "url": "/static/media/14.aba1ad49.jpg"
   },
   {
     "revision": "268b37ee91663ae4284b36d240f46f17",
-    "url": "/exchange/static/media/15.268b37ee.jpg"
+    "url": "/static/media/15.268b37ee.jpg"
   },
   {
     "revision": "88558501acdcdd4152b938fd563279e9",
-    "url": "/exchange/static/media/16.88558501.jpg"
+    "url": "/static/media/16.88558501.jpg"
   },
   {
     "revision": "2c5f3dd9f79229341aafa7765cccf176",
-    "url": "/exchange/static/media/17.2c5f3dd9.jpg"
+    "url": "/static/media/17.2c5f3dd9.jpg"
   },
   {
     "revision": "1bd5929c6e610c2c4b91fa03d111a899",
-    "url": "/exchange/static/media/18.1bd5929c.jpg"
+    "url": "/static/media/18.1bd5929c.jpg"
   },
   {
     "revision": "414ca2cea05e8c87e7b32090ec15a2d1",
-    "url": "/exchange/static/media/19.414ca2ce.jpg"
+    "url": "/static/media/19.414ca2ce.jpg"
   },
   {
     "revision": "b7215194cc02f25d3be75529a3302d96",
-    "url": "/exchange/static/media/2.b7215194.jpg"
+    "url": "/static/media/2.b7215194.jpg"
   },
   {
     "revision": "10ff1cef356e504399db09b1a49f5857",
-    "url": "/exchange/static/media/20.10ff1cef.jpg"
+    "url": "/static/media/20.10ff1cef.jpg"
   },
   {
     "revision": "92a2a19fc5ec8efa0d34678ac8979a57",
-    "url": "/exchange/static/media/21.92a2a19f.jpg"
+    "url": "/static/media/21.92a2a19f.jpg"
   },
   {
     "revision": "7c77377730e6fc9e9a7e0b54c4ec142a",
-    "url": "/exchange/static/media/22.7c773777.jpg"
+    "url": "/static/media/22.7c773777.jpg"
   },
   {
     "revision": "e8b3930f3844946a37f328df6de66006",
-    "url": "/exchange/static/media/23.e8b3930f.jpg"
+    "url": "/static/media/23.e8b3930f.jpg"
   },
   {
     "revision": "003b80ec17d91564144555350b261f1a",
-    "url": "/exchange/static/media/24.003b80ec.jpg"
+    "url": "/static/media/24.003b80ec.jpg"
   },
   {
     "revision": "5dca5cd22545346550fcc5d6da29c36a",
-    "url": "/exchange/static/media/25.5dca5cd2.jpg"
+    "url": "/static/media/25.5dca5cd2.jpg"
   },
   {
     "revision": "ad33005fa0211b67c7be933ec6133690",
-    "url": "/exchange/static/media/26.ad33005f.jpg"
+    "url": "/static/media/26.ad33005f.jpg"
   },
   {
     "revision": "7ae90821099d27270fe8d2c1e21dde4a",
-    "url": "/exchange/static/media/27.7ae90821.jpg"
+    "url": "/static/media/27.7ae90821.jpg"
   },
   {
     "revision": "e5fb6d5a70e0fb45bcee658db72c4956",
-    "url": "/exchange/static/media/28.e5fb6d5a.jpg"
+    "url": "/static/media/28.e5fb6d5a.jpg"
   },
   {
     "revision": "0c95cfe7eeb7e4697408b8f3562b7454",
-    "url": "/exchange/static/media/29.0c95cfe7.jpg"
+    "url": "/static/media/29.0c95cfe7.jpg"
   },
   {
     "revision": "d8e1d4c4093a3af858ff595d0ac75c15",
-    "url": "/exchange/static/media/3.d8e1d4c4.jpg"
+    "url": "/static/media/3.d8e1d4c4.jpg"
   },
   {
     "revision": "1f7585df839cc5805bc6b6a96cec4232",
-    "url": "/exchange/static/media/30.1f7585df.jpg"
+    "url": "/static/media/30.1f7585df.jpg"
   },
   {
     "revision": "04d7d420a0eea92ba3299e8393070f7f",
-    "url": "/exchange/static/media/31.04d7d420.jpg"
+    "url": "/static/media/31.04d7d420.jpg"
   },
   {
     "revision": "83470118125b34bf152120062aefe401",
-    "url": "/exchange/static/media/32.83470118.jpg"
+    "url": "/static/media/32.83470118.jpg"
   },
   {
     "revision": "4c0abc0fd43c47783d0727a6df75d8fa",
-    "url": "/exchange/static/media/33.4c0abc0f.jpg"
+    "url": "/static/media/33.4c0abc0f.jpg"
   },
   {
-    "revision": "8a48e0828459d7e06e6c",
-    "url": "/exchange/static/js/main.67196e1c.chunk.js"
+    "revision": "da1530b5107feacff600",
+    "url": "/static/js/main.fc16c189.chunk.js"
   },
   {
     "revision": "05e9893af61a2f446067fbd5c79db386",
-    "url": "/exchange/static/media/35.05e9893a.jpg"
+    "url": "/static/media/35.05e9893a.jpg"
   },
   {
     "revision": "fe3f1b1e609cd5c97c4a2b449eaea9d0",
-    "url": "/exchange/static/media/36.fe3f1b1e.jpg"
+    "url": "/static/media/36.fe3f1b1e.jpg"
   },
   {
     "revision": "65a30faf2bf48e7c63ce4266232687f5",
-    "url": "/exchange/static/media/37.65a30faf.jpg"
+    "url": "/static/media/37.65a30faf.jpg"
   },
   {
     "revision": "08ebbf37bef2ab63151ee007ff40f186",
-    "url": "/exchange/static/media/38.08ebbf37.jpg"
+    "url": "/static/media/38.08ebbf37.jpg"
   },
   {
     "revision": "4595f3726d9ac01fed312db4a7abd632",
-    "url": "/exchange/static/media/39.4595f372.jpg"
+    "url": "/static/media/39.4595f372.jpg"
   },
   {
     "revision": "8c41faa485adcf086408ee2887fe78f5",
-    "url": "/exchange/static/media/4.8c41faa4.jpg"
+    "url": "/static/media/4.8c41faa4.jpg"
   },
   {
     "revision": "5c55c15b5c728cc87c878139808f7590",
-    "url": "/exchange/static/media/40.5c55c15b.jpg"
+    "url": "/static/media/40.5c55c15b.jpg"
   },
   {
     "revision": "f77f14a7c3a765bc69963b585edd3c31",
-    "url": "/exchange/static/media/41.f77f14a7.jpg"
+    "url": "/static/media/41.f77f14a7.jpg"
   },
   {
     "revision": "fe4ff2d97c0e30b04f808157d211a8df",
-    "url": "/exchange/static/media/42.fe4ff2d9.jpg"
+    "url": "/static/media/42.fe4ff2d9.jpg"
   },
   {
     "revision": "56ce154fcfa2fd3b93008c9e43a6a56a",
-    "url": "/exchange/static/media/43.56ce154f.jpg"
+    "url": "/static/media/43.56ce154f.jpg"
   },
   {
     "revision": "c65560fa72ae9a5131fe2caa62c509ec",
-    "url": "/exchange/static/media/44.c65560fa.jpg"
+    "url": "/static/media/44.c65560fa.jpg"
   },
   {
     "revision": "8e85ff4915afea60c223a4edfc4109d3",
-    "url": "/exchange/static/media/45.8e85ff49.jpg"
+    "url": "/static/media/45.8e85ff49.jpg"
   },
   {
     "revision": "f4a7c44b1f899658e3566ce3145ad26f",
-    "url": "/exchange/static/media/46.f4a7c44b.jpg"
+    "url": "/static/media/46.f4a7c44b.jpg"
   },
   {
     "revision": "7a2610a1af44aaba56ca0ac2c87b87e6",
-    "url": "/exchange/static/media/47.7a2610a1.jpg"
+    "url": "/static/media/47.7a2610a1.jpg"
   },
   {
     "revision": "ec1b3ebe1229d3096289c4e1896ec5c1",
-    "url": "/exchange/static/media/48.ec1b3ebe.jpg"
+    "url": "/static/media/48.ec1b3ebe.jpg"
   },
   {
     "revision": "91f630cf78177d3ba9af820a5667edb6",
-    "url": "/exchange/static/media/49.91f630cf.jpg"
+    "url": "/static/media/49.91f630cf.jpg"
   },
   {
     "revision": "bf982959e489025106aabf29b62830d1",
-    "url": "/exchange/static/media/5.bf982959.jpg"
+    "url": "/static/media/5.bf982959.jpg"
   },
   {
     "revision": "593eba029bb1f1c5d1390e5d3e1f12d2",
-    "url": "/exchange/static/media/50.593eba02.jpg"
+    "url": "/static/media/50.593eba02.jpg"
   },
   {
     "revision": "70a518f24379217c676a6cb9e93285b9",
-    "url": "/exchange/static/media/51.70a518f2.jpg"
+    "url": "/static/media/51.70a518f2.jpg"
   },
   {
     "revision": "79bbeffce8eb307f03eac494c86eacaa",
-    "url": "/exchange/static/media/52.79bbeffc.jpg"
+    "url": "/static/media/52.79bbeffc.jpg"
   },
   {
     "revision": "e03e146376392239ede38e4d7e7b25ca",
-    "url": "/exchange/static/media/53.e03e1463.jpg"
+    "url": "/static/media/53.e03e1463.jpg"
   },
   {
     "revision": "c0da7ad8966eac0810a401527e0b9b70",
-    "url": "/exchange/static/media/54.c0da7ad8.jpg"
+    "url": "/static/media/54.c0da7ad8.jpg"
   },
   {
     "revision": "50653493ef1872f5c44755b4f841982f",
-    "url": "/exchange/static/media/55.50653493.jpg"
+    "url": "/static/media/55.50653493.jpg"
   },
   {
     "revision": "3706cd9919bcf1fbaefa9a6ddf36695e",
-    "url": "/exchange/static/media/56.3706cd99.jpg"
+    "url": "/static/media/56.3706cd99.jpg"
   },
   {
     "revision": "9d1feee9d40ca60ef0a20588145a5790",
-    "url": "/exchange/static/media/57.9d1feee9.jpg"
+    "url": "/static/media/57.9d1feee9.jpg"
   },
   {
     "revision": "b6beac5b4ecd8dd773cc7cffa20cd535",
-    "url": "/exchange/static/media/58.b6beac5b.jpg"
+    "url": "/static/media/58.b6beac5b.jpg"
   },
   {
     "revision": "7e8b16978ddc2b3144117485ef1f4dcb",
-    "url": "/exchange/static/media/59.7e8b1697.jpg"
+    "url": "/static/media/59.7e8b1697.jpg"
   },
   {
     "revision": "d129c9e448e6af398d8f188c77b38acd",
-    "url": "/exchange/static/media/6.d129c9e4.jpg"
+    "url": "/static/media/6.d129c9e4.jpg"
   },
   {
     "revision": "c310ec96a279a9565e5af2e69dd6d08c",
-    "url": "/exchange/static/media/7.c310ec96.jpg"
+    "url": "/static/media/7.c310ec96.jpg"
   },
   {
     "revision": "fa8530f30ceac853fb50f3a4764a7811",
-    "url": "/exchange/static/media/8.fa8530f3.jpg"
+    "url": "/static/media/8.fa8530f3.jpg"
   },
   {
     "revision": "61cd67bef9a7a1c6fa956f64abbe007c",
-    "url": "/exchange/static/media/9.61cd67be.jpg"
+    "url": "/static/media/9.61cd67be.jpg"
   },
   {
-    "revision": "83517c9d8fb9648a589a68a5f53a5a40",
-    "url": "/exchange/index.html"
+    "revision": "2f82d2d5c400421391aca41b667299d7",
+    "url": "/index.html"
   }
 ];
