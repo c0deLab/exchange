c64be7ce63dd2819ffd8e8ee3b9e567f189e8060
taliaperry <70407959+taliaperry@users.noreply.github.com>
2020-09-03 22:11:31 -0400

    creating all the pages

---
+++ scripts/landscape_warning.js
@@ -0,0 +1,3 @@
+if(window.innerHeight > window.innerWidth){
+alert("We recommend switching to landscape mode, or the maximizing your browser width, for a better experience. If the gallery image does not fill your screen, zoom your browser accordingly.");
+}
